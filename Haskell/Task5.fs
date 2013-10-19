0bef42c816b72b0658bcc69a4dcd901576dd5317
Nikita Batov <nikitabatov@gmail.com>
2013-10-19 18:28:27 +0400

    fix pattern match failure.Done.

--- Haskell/Task5.fs
+++ Haskell/Task5.fs
@@ -1,25 +1,13 @@
 data Tree a = Empty | Node a Integer (Tree a) (Tree a) deriving Show
 
 insert Empty x = Node x 1 Empty Empty
-insert (Node a h l r) x
+insert nd@(Node a h l r) x
 	| a > x = Node a h (insert l x) r
 	| a < x = Node a h l (insert r x)
-	| otherwise = Node a h l r
+	| otherwise = nd
 
 
-
-getH Empty = 0
-getH (Node _ h _ _) = h
-
-setH Empty = Empty
-setH (Node a _ l r) = Node a (max (getH l) (getH r) +1) l r
-
-lR (Node a h (Node al hl ll lr) r) = setH (Node al 0 ll (setH (Node a 0 lr r))) 
-rR (Node a h l (Node ar hr rl rr)) = setH (Node ar 0 (setH (Node a 0 l rl)) rr)
-
-dbh (Node _ _ l r) = (getH l) - (getH r)
-dbh Empty = 0
-
+-- 4 type of rotate
 balance nd@(Node a h l r)
 	| dbh nd -1 > 0 && dbh r >  0 = setH $ lR nd
 	| dbh nd -1 > 0 && dbh r <= 0 = setH $ lR (Node a h l (rR r))
@@ -27,3 +15,18 @@ balance nd@(Node a h l r)
 	| dbh nd +1 < 0 && dbh r >= 0 = setH $ rR (Node a h (lR l) r)
 	| otherwise = nd
 
+  where
+	getH (Node _ h _ _) = h
+	getH Empty = 0
+
+	setH (Node a _ l r) = Node a (max (getH l) (getH r) +1) l r
+	setH Empty = Empty
+
+	lR (Node a h (Node al hl ll lr) r) = setH (Node al 0 ll (setH (Node a 0 lr r)))
+	lR Empty = Empty 
+	rR (Node a h l (Node ar hr rl rr)) = setH (Node ar 0 (setH (Node a 0 l rl)) rr)
+	rR Empty = Empty
+
+	dbh (Node _ _ l r) = (getH l) - (getH r)	
+	dbh Empty = 0
+	
