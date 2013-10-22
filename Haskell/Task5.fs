2b491c7ce87c25d1c6cd1656577611d0406b9a05
Nikita Batov <nikitabatov@gmail.com>
2013-10-22 13:21:30 +0400

    fix

--- Haskell/Task5.fs
+++ Haskell/Task5.fs
@@ -2,25 +2,26 @@ data Tree a = Empty | Node a Integer (Tree a) (Tree a) deriving Show
 
 insert Empty x = Node x 1 Empty Empty
 insert nd@(Node a h l r) x
-	| a > x = Node a h (insert l x) r
-	| a < x = Node a h l (insert r x)
+	| a > x = setH (Node a h (insert l x) r)
+	| a < x = setH (Node a h l (insert r x))
 	| otherwise = nd
 
+setH (Node a _ l r) = Node a (max (getH l) (getH r) +1) l r
+setH Empty = Empty
+
+getH (Node _ h _ _) = h
+getH Empty = 0
 
 -- 4 type of rotate
 balance nd@(Node a h l r)
-	| dbh nd -1 > 0 && dbh r >  0 = setH $ lR nd
-	| dbh nd -1 > 0 && dbh r <= 0 = setH $ lR (Node a h l (rR r))
-	| dbh nd +1 < 0 && dbh l <  0 = setH $ rR nd
-	| dbh nd +1 < 0 && dbh r >= 0 = setH $ rR (Node a h (lR l) r)
+	| (dbh nd)-1 > 0 && dbh r >  0 = lR nd
+	| (dbh nd)-1 > 0 && dbh r <= 0 = lR (Node a h l (rR r))
+	| (dbh nd)+1 < 0 && dbh l <  0 = rR nd
+	| (dbh nd)+1 < 0 && dbh l >= 0 = rR (Node a h (lR l) r)
 	| otherwise = nd
 
   where
-	getH (Node _ h _ _) = h
-	getH Empty = 0
-
-	setH (Node a _ l r) = Node a (max (getH l) (getH r) +1) l r
-	setH Empty = Empty
+		
 
 	lR (Node a h (Node al hl ll lr) r) = setH (Node al 0 ll (setH (Node a 0 lr r)))
 	lR Empty = Empty 
