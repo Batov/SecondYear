18862ad99d81c188302313f8a1d8a5b473f24f25
Nikita Batov <nikitabatov@yandex.ru>
2013-12-08 15:29:25 +0300

    refactoring...

--- F#/RE.fs
+++ F#/RE.fs
@@ -1,4 +1,4 @@
-﻿module RE.Main
+module RE.Main
 
 open System.Text.RegularExpressions
 open System
@@ -8,21 +8,24 @@ let domain_name = "([a-z0-9]([a-z0-9]{0,61}[a-zA-Z0-9])?\.)*(aero|arpa|asia|biz|
 let pattern = user_name + "@" + domain_name
 
 let regexp = new Regex(pattern) 
-let matcher input = regexp.IsMatch(input)  
+let matcher input = regexp.IsMatch(input) 
+let tester input ans = if (matcher input) <> ans then printf "%A parser failed! Should be %A \n" input ans   
 
-let tester = 
-  if matcher "a@b.cc" = true then printfn "%A" "OK" else printfn "%A" "FAILED"
-  if matcher "victor.polozov@mail.ru" = true then printfn "%A" "OK" else printfn "%A" "FAILED"  
-  if matcher "my@domain.info" = true then printfn "%A" "OK" else printfn "%A" "FAILED"  
-  if matcher "_.1@mail.com" = true then printfn "%A" "OK" else printfn "%A" "FAILED"  
-  if matcher "coins_department@hermitage.museum" = true then printfn "%A" "OK" else printfn "%A" "FAILED"  
-  
-  if matcher "a@b.c" = false then printfn "%A" "OK" else printfn "%A" "FAILED"  
-  if matcher "a..b@mail.ru" = false then printfn "%A" "OK" else printfn "%A" "FAILED"  
-  if matcher ".a@mail.ru" = false then printfn "%A" "OK" else printfn "%A" "FAILED"    
-  if matcher "yo@domain.somedomain" = false then printfn "%A" "OK" else printfn "%A" "FAILED" 
-  if matcher "1@mail.ru" = false then printfn "%A" "OK" else printfn "%A" "FAILED"    
-  
+tester "victor.polozov@gmail.ru" true
+tester "a@b.cc" true
+tester "my@domain.info" true
+tester "_.1@mail.com" true
+tester "coins_department@hermitage.museum" true
+
+tester "a@b.c" false
+tester "a..b@mail.ru" false
+tester ".a@mail.ru" false
+tester "yo@domain.somedomain" false
+tester "1@mail.ru" false
+
+printfn "Testing is finished..."
+
+(* 
   [<TestClass>]
    type UnitTest() =
     [<TestMethod>]
@@ -64,4 +67,4 @@ let tester =
     [<TestMethod>]
     member x.TestMethod10 () =
         let testVal = matcher "1@mail.ru"
-        Assert.IsFalse(testVal)
+        Assert.IsFalse(testVal) *)
