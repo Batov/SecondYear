c052c42b5ed6a65b5f1121bcaa0e34c5afa78b27
Nikita Batov <nikitabatov@yandex.ru>
2013-12-12 13:55:52 +0300

    Update RE.fs

--- F#/RE.fs
+++ F#/RE.fs
@@ -1,9 +1,9 @@
-module RE.Main
+﻿module RE.Main
 
 open System.Text.RegularExpressions
 open System
 
-let user_name = "^[-a-z!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*"
+let user_name = "^[-a-zA-Z!#$%&'*+/=?^_`{|}~]+(\.[-a-zA-Z0-9!#$%&'*+/=?^_`{|}~]+)*"
 let domain_name = "([a-z0-9]([a-z0-9]{0,61}[a-zA-Z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|xxx|[a-z][a-z])$"
 let pattern = user_name + "@" + domain_name
 
@@ -11,13 +11,14 @@ let regexp = new Regex(pattern)
 let matcher input = regexp.IsMatch(input) 
 let tester input ans = if (matcher input) <> ans then printf "%A parser failed! Should be %A \n" input ans   
 
-tester "victor.polozov@gmail.ru" true
+tester "Victor.Polozov@gmail.ru" true
 tester "a@b.cc" true
-tester "my@domain.info" true
+tester "mY@domain.info" true
 tester "_.1@mail.com" true
 tester "coins_department@hermitage.museum" true
 
 tester "a@b.c" false
+tester "abracadabra@B.com" false
 tester "a..b@mail.ru" false
 tester ".a@mail.ru" false
 tester "yo@domain.somedomain" false
